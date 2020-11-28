ad746f176ef07b8af2dd8824eb5a396c653a3a14
Vanya <56477758+Vanya1808@users.noreply.github.com>
2020-11-28 18:08:07 +0300

    2

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
-"use strict";
-
-import gallery from "./gallery-items.js";
-
-console.log(gallery);
-const list = document.querySelector(".js-gallery");
-const div1 = document.querySelector(".js-lightbox");
-const image = document.querySelector(".lightbox__image");
-const button = document.querySelector(".lightbox__button");
-console.log(div1, image, button);
-const generateGallery = function (array, place) {
-  return array.map((elem, index) => {
-    let keys = Object.keys(elem);
-    const item = document.createElement("li");
-    const link = document.createElement("a");
-    link.classList.add("gallery__link");
-    link.setAttribute("href", "");
-    const img = document.createElement("img");
-    img.classList.add("gallery__image");
-    img.setAttribute("src", elem.preview);
-    img.setAttribute("data-source", elem.original);
-    img.setAttribute("alt", elem.description);
-    img.setAttribute("data-index", index);
-    link.appendChild(img);
-    item.appendChild(link);
-    place.appendChild(item);
-  });
-};
-generateGallery(gallery, list);
-
-// МОДАЛЬНОЕ ОКНО
-
-list.addEventListener("click", (event) => {
-  event.preventDefault();
-  toOpenModal(event);
-});
-
-function toOpenModal(event) {
-  div1.classList.add("is-open");
-  if (event.target.nodeName === "IMG") {
-    let path = event.target.dataset.source;
-    image.src = path;
-    console.log(path);
+import gallery from "./gallery-items.js"
+const listGallery = document.querySelector('.js-gallery')
+const lightbox = document.querySelector('.lightbox')
+const closeLightbox = document.querySelector('.lightbox__button')
+const lightboxImage = document.querySelector('.lightbox__image')
+listGallery.addEventListener('click', (event)=>{
+  if(event.target.tagName === 'IMG') {
+    event.preventDefault()
+    lightbox.classList.add('is-open')
+    const modalImageSrc =  event.target.dataset.source
+    lightboxImage.setAttribute('src', modalImageSrc)
+    console.dir(event.target)
   }
-  return;
-}
+})
+closeLightbox.addEventListener('click', (e) => {
+  lightbox.classList.remove('is-open')
+  lightboxImage.removeAttribute('src')
+})
+console.log(gallery)
 
-function toCloseModal() {
-  div1.classList.remove("is-open");
-  image.src = "";
-}
-button.addEventListener("click", toCloseModal);
+gallery.map((obj, i, arr)=>{
+  console.log(obj);
+  const imgPrewiewGallery = document.createElement('img');
+  imgPrewiewGallery.setAttribute('src', obj.preview)
+  imgPrewiewGallery.setAttribute('alt', obj.description)
+  imgPrewiewGallery.classList.add('gallery__image')
+  imgPrewiewGallery.setAttribute('data-source', obj.original)
+  imgPrewiewGallery.setAttribute('data-value', i)
+  console.log(imgPrewiewGallery)
+
+  const linkGallery = document.createElement('a')
+  linkGallery.classList.add('gallery-link')
+  linkGallery.setAttribute('href', obj.original)
+
+  linkGallery.appendChild(imgPrewiewGallery)
+
+  const galleryItem = document.createElement('li')
+  galleryItem.classList.add('gallery_item')
+
+  galleryItem.appendChild(linkGallery)
+  console.log(galleryItem);
+
+  listGallery.appendChild(galleryItem)
+});
